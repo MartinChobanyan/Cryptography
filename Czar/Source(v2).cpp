36496fdef583d91701ab73b0b5c9f73aa8eb2ebc
Martin Chobanyan <foxmart36@gmail.com>
2018-06-13 23:40:50 +0400

    Update Source(v2).cpp

--- Czar/Source(v2).cpp
+++ Czar/Source(v2).cpp
@@ -6,37 +6,45 @@
 
 using namespace std;
 
-string Czar_Cryption(string&,const int&);
+string Czar_Cryption(string&, const int&);
 
+ifstream fin("text.txt");
 string word;
 int key;
-ifstream fin("text.txt");
-ofstream fout("output.txt");
+char ok;
 
 int main() {
-	cout << "This is realisation of the Czar cryption algorithm!\nBefore using the program, you must create a text file(text.txt) in the same program folder.\nCryption -> + key\nEncryption -> - key\n";
-	cout << "Input the key: ";
-	cin >> key;
-	// Crypt part
-	fin >> word;
-	fout << Czar_Cryption(word, key);
-	while (!fin.eof())
-	{
+	do{
+		ofstream fout("o.temp"); // temp file
+		system("cls"); // clearing console area
+		//instruction
+		cout << "This is realisation of the Czar cryption algorithm!\nBefore using the program, you must create a text file(text.txt) in the same program folder.\nCryption -> + key\nEncryption -> - key\n";
+		// key input
+		cout << "Input the key: ";
+		cin >> key;
+		// Crypt part
 		fin >> word;
-		fout << " " << Czar_Cryption(word, key);
-	} 
-	//closing processes with files
-	fin.close();
-	fout.close();
-	// operations with files
-	if (remove("text.txt") & rename("output.txt", "text.txt")) cout << "Something had gone wrong!"; else cout << "Succes!";
-	cout << endl;
-	// end
+		fout << Czar_Cryption(word, key);
+		while (!fin.eof())
+		{
+			fin >> word;
+			fout << " " << Czar_Cryption(word, key);
+		}
+		//Closing processes with files
+		fin.close();
+		fout.close();
+		//Operations with files
+		if (!remove("text.txt") && !rename("o.temp", "text.txt")) cout << "Succes!"; else cout << "Something had gone wrong!";
+		// Repeating? part
+		cout << "\n Do you want to repeat?(y/n): ";
+		cin >> ok;
+	} while (tolower(ok) == 'y');
 	system("pause");
 	return 0;
 }
 
-string Czar_Cryption(string& word,const int& key) {
-	for (int i = 0; i < (int)word.size(); i++) word[i] = (word[i] > 64 && word[i] < 91) || (word[i] > 96 && word[i] < 123) ? islower(word[i]) ? (word[i] - 19 + key) % 26 + 'a' : (word[i] - 13 + key) % 26 + 'A':word[i];
+// Encrypt/Decrypt function
+string Czar_Cryption(string& word, const int& key) {
+	for (int i = 0; i < (int)word.size(); i++) word[i] = (word[i] > 64 && word[i] < 91) || (word[i] > 96 && word[i] < 123) ? islower(word[i]) ? (word[i] - 19 + key) % 26 + 'a' : (word[i] - 13 + key) % 26 + 'A' : word[i];
 	return word;
 }
