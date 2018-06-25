4e3760b0b5cdda75fe298921fedad580ea9e61b2
Martin Chobanyan <foxmart36@gmail.com>
2018-06-25 20:04:28 +0400

    #FIXING little bug in Grosnfeld cipher algorithm.

--- Czar/Source(v4).cpp
+++ Czar/Source(v4).cpp
@@ -7,43 +7,43 @@
 using namespace std;
 
 inline char Czar_Cryption(char&, const int&); // Czar cryption
-inline char Hronsfeld_Cryption(char&, const string&); // Hronsfeld Cryption
+inline char Gronsfeld_Cryption(char&, const string&); // Gronsfeld Cryption
 
-string keyh; // Hronsfeld 
+string keyg; // Gronsfeld 
 int keyc; // Czar key
 char ch, ok, mode; // file input stream symbols reader // Loop accepter // Crypt/encrypt mode reader
 
-unsigned long long int i; // kostil ((((MAYBE)))) before better realisation of hronsfeld crypt algorithm // TODO: CHANGE!
+unsigned long long int i; // kostil ((((MAYBE)))) before better realisation of Gronsfeld crypt algorithm // TODO: CHANGE!
 
 int main() {
-	do{
-		::i = 0; // for hronsfeld char counter RESET OPERATION // TODO:CHANGE! 
+	do {
+		::i = 0; // for Gronsfeld char counter RESET OPERATION // TODO:CHANGE! 
 
 		system("cls"); // Clearing console area
 		while (!ifstream("text.txt")) {
-			cout << "WARNING!!!: Before using the program, you must create a text file \"text.txt\" in the same program folder.\n";
+			cout << "WARNING!!!: To continue, you must create a text file \"text.txt\" in the same program folder and press Enter.\n";
 			system("pause");
 			system("cls"); // Warn clearing
 		}
 		ifstream fin("text.txt"); // Main file
 		ofstream fout("o.temp"); // Temp file
 		//instruction
-		cout << "This is realisation of the Czar and Hronsfeld cryption algorithm!\nChoose the crypt algorithm mode c(CZAR)/h(HRONSFELD).\nCryption -> + key\nEncryption -> - key" << endl;
+		cout << "This is the implementation of encryption algorithms Czar and Greensfeld!\nSelect the encryption algorithm c(CZAR)/g(GRONSFELD).\nEncryption -> + key\nDecryption -> - key" << endl;
 		// Mode selection
 		do {
-			cout << "Select mode Czar/Hronsfeld crypt algorithm(c/h): ";
+			cout << "Select mode Czar/Gronsfeld crypt algorithm(c/h): ";
 			cin >> mode;
 			mode = tolower(mode);
-		} while (mode != 'c' && mode != 'h');
-		
+		} while (mode != 'c' && mode != 'g');
+
 		cout << "Input the key: ";
-		if(mode=='c') 
+		if (mode == 'c')
 			cin >> keyc; // Czar key input 
-		else 
-			cin >> keyh; // Hronsfeld key input
+		else
+			cin >> keyg; // Gronsfeld key input
 		// Crypt part
-		while (!fin.eof() && fin.get(ch)) 
-			fout.put(mode == 'c' ? Czar_Cryption(ch, keyc) : Hronsfeld_Cryption(ch, keyh));
+		while (!fin.eof() && fin.get(ch))
+			fout.put(mode == 'c' ? Czar_Cryption(ch, keyc) : Gronsfeld_Cryption(ch, keyh));
 		// Closing processes with files
 		fin.close();
 		fout.close();
@@ -59,10 +59,10 @@ int main() {
 
 // Encrypt/Decrypt functions
 
-inline char Czar_Cryption(char& symbol, const int& key) { 
+inline char Czar_Cryption(char& symbol, const int& key) {
 	return (symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123) ? islower(symbol) ? (symbol - 19 + key) % 26 + 'a' : (symbol - 13 + key) % 26 + 'A' : symbol;
 }
 
-inline char Hronsfeld_Cryption(char& symbol, const string& key) {
-	return (symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123) ? (key[0] > 47 && key[0] < 57) || (key[0] == '+' || key[0] == '-') ? Czar_Cryption(symbol, key[0] != '+' && key[0] != '-' ? key[::i++ % (int)key.size()] - '0' : key[0] == '+' ? key[::i++ % ((int)key.size() - 1) + 1] - '0' : -(key[::i++ % ((int)key.size() - 1) + 1] - '0')) : '#' : symbol;
+inline char Gronsfeld_Cryption(char& symbol, const string& key) {
+	return (symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 123) ? (key[0] > 47 && key[0] < 58) || (key[0] == '+' || key[0] == '-') ? Czar_Cryption(symbol, (key[0] != '+' && key[0] != '-') ? key[::i++ % (int)key.size()] - '0' : (key[0] == '+') ? key[::i++ % ((int)key.size() - 1) + 1] - '0' : -(key[::i++ % ((int)key.size() - 1) + 1] - '0')) : '#' : symbol;
 }
